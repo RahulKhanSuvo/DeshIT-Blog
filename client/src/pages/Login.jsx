9b133eec3e782ef8de542e4517fb0184ff8771bd
RahulKhanSuvo <rahul.khan.suvo@gmailcom>
2025-09-29 12:26:33 +0600

    feat: implement login page layout with form and logo

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,37 @@
+import logo from "../assets/brand_logo.svg";
 const Login = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="bg-white min-w-[440px] p-12 rounded-lg shadow-lg flex flex-col items-center gap-4">
+        <img src={logo} alt="" />
+        <p>Welcome Newsx, Please Log In</p>
+        <form className="w-full flex flex-col gap-6 mt-4">
+          <div className="flex flex-col gap-2 w-full ">
+            <label htmlFor="email">Email</label>
+            <input
+              placeholder="abc@example.com"
+              name="email"
+              type="email"
+              className="h-11 bg-[#E3E3E3] px-5 py-6 border-none rounded-md focus:border-none outline-none text-"
+            />
+          </div>
+          <div className="flex flex-col gap-2 w-full ">
+            <label htmlFor="password">Password</label>
+            <input
+              placeholder="••••••••"
+              name="password"
+              type="password"
+              className="h-11 bg-[#E3E3E3] px-5 py-6 border-none rounded-md 
+             focus:outline-none text-gray-800 placeholder-gray-500"
+            />
+          </div>
+          <button className="bg-[#6200EE] w-full rounded-md py-3 text-white">
+            Login
+          </button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
